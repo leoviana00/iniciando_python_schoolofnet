25cdf6fcad096df96bf8f298e7104364845701f3
Leonardo Viana <leonardoviana00@hotmail.com>
2021-05-27 22:01:23 -0300

    trabalhando com while

---
+++ while.py
@@ -0,0 +1,16 @@
+#Trabalhando com while
+
+number = 5
+
+while number < 10:
+    print(number)
+    number += 1
+    
+    '''  
+    if number == 8:
+        print("Break!!!")
+        break
+    '''
+else:
+    print('Not true anymore')
+
